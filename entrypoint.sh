a66f4948023d56e9bc89db9de3f6eee9020d2745
vaggeliskls <vaggeliskls@turintech.ai>
2025-02-01 14:49:40 +0200

    feat: dynamic entrypoint script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
-exec npx docs-to-pdf $DEFAULT_ARGS "$@"
+CMD="npx docs-to-pdf $DEFAULT_ARGS --initialDocURLs=\"$DOCS_URL\" --contentSelector=\"$CONTENT_SELECTOR\" --paginationSelector=\"$PAGINATION_SELECTOR\" --excludeSelectors=\"$EXCLUDE_SELECTOR\" --outputPDFFilename=\"$OUTPUT_PDF_FILENAME\""
+
+if [ -n "$EXCLUDE_PATHS" ]; then
+  CMD="$CMD --excludePaths=\"$EXCLUDE_PATHS\""
+fi
+
+if [ -n "$COVER_TITLE" ]; then
+  CMD="$CMD --coverTitle=\"$COVER_TITLE\""
+fi
+
+# Execute the final command with all arguments
+eval "$CMD" "$@"
