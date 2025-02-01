739507a1583de2665b26d05713df61f22fda25c2
vaggeliskls <vaggeliskls@turintech.ai>
2025-02-01 17:21:07 +0200

    feat: add more envs on entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,24 @@
-#!/bin/sh
-CMD="npx docs-to-pdf $DEFAULT_ARGS --initialDocURLs=\"$DOCS_URL\" --contentSelector=\"$CONTENT_SELECTOR\" --paginationSelector=\"$PAGINATION_SELECTOR\" --excludeSelectors=\"$EXCLUDE_SELECTOR\" --outputPDFFilename=\"$OUTPUT_PDF_FILENAME\""
+#!/bin/bash
 
-if [ -n "$EXCLUDE_PATHS" ]; then
-  CMD="$CMD --excludePaths=\"$EXCLUDE_PATHS\""
-fi
+# Initialize the command array
+CMD=("docs-to-pdf")
 
-if [ -n "$COVER_TITLE" ]; then
-  CMD="$CMD --coverTitle=\"$COVER_TITLE\""
-fi
+# Add required arguments
+CMD+=("--initialDocURLs=\"$DOCS_URL\"")
+CMD+=("--outputPDFFilename=\"$OUTPUT_PDF_FILENAME\"")
 
-# Execute the final command with all arguments
-eval "$CMD" "$@"
+# Add optional arguments if they are defined
+[ -n "$PUPPETEER_ARGS" ] && CMD+=("--puppeteerArgs=\"$PUPPETEER_ARGS\"")
+[ -n "$EXCLUDE_URLS" ] && CMD+=("--excludeURLs=\"$EXCLUDE_URLS\"")
+[ -n "$EXLUDE_PATHS" ] && CMD+=("--excludePaths=\"$EXLUDE_PATHS\"")
+[ -n "$COVER_TITLE" ] && CMD+=("--coverTitle=\"$COVER_TITLE\"")
+[ -n "$CONTENT_SELECTOR" ] && CMD+=("--contentSelector=\"$CONTENT_SELECTOR\"")
+[ -n "$PAGINATION_SELECTOR" ] && CMD+=("--paginationSelector=\"$PAGINATION_SELECTOR\"")
+[ -n "$EXCLUDE_SELECTOR" ] && CMD+=("--excludeSelectors=\"$EXCLUDE_SELECTOR\"")
+[ -n "$EXTRA_ARGS" ] && CMD+=("$EXTRA_ARGS")
+
+# Print the full command for debugging
+echo "Executing command: ${CMD[@]} $@"
+
+# Execute the command with additional arguments passed to the script
+eval "${CMD[@]}" "$@"
